5739f4673d53e71dd6316181da2ce5f536feba23
James <jameskmonger@hotmail.co.uk>
2015-11-02 18:41:58 +0000

    Fixed routing to tiles

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = require('./config');
 
 shortid.characters("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_");
 
-app.use("/assets/img", express.static(__dirname + '/assets/img'));
+app.use("/assets/tiles", express.static(__dirname + '/assets/tiles'));
 
 app.use("/lib", express.static(__dirname + '/lib'));
 

bc109d66b908f7845a4c420188884878214c53c5
James Monger <jameskmonger@hotmail.co.uk>
2015-11-22 20:04:38 +0000

    Moved index.html to build folder

--- src/ts/server.ts
+++ src/ts/server.ts
@@ -5,7 +5,6 @@ var express = require('express');
 var app = express();
 
 app.use("/assets", express.static(__dirname + "/assets"));
-
 app.use("/lib", express.static(__dirname + '/lib'));
 
 app.get('/', function(req, res) {

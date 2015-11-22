d5f2792e5cddf63cd9a22a70427596183db75a27
James Monger <jameskmonger@hotmail.co.uk>
2015-11-22 13:08:34 +0000

    Startedw ork on server (configured build path)

---
+++ build/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use("/lib", express.static(__dirname + '/lib'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get("/script/:filename", function (req, res) {
+    var filename = req.params.filename;
+    res.sendFile(__dirname + '/script/' + filename);
+});
+app.get("/script/app/:filename", function (req, res) {
+    var filename = req.params.filename;
+    res.sendFile(__dirname + '/script/app/' + filename);
+});
+app.get("/script/app/:folder/:filename", function (req, res) {
+    var folder = req.params.folder;
+    var filename = req.params.filename;
+    if (folder === 'Animation' || folder === 'Networking' || folder === 'World') {
+        res.sendFile(__dirname + '/script/app/' + folder + '/' + filename);
+    }
+});
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});

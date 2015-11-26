5c4339289f3e751467460fda283fed22ad8890e9
Jameskmonger <jameskmonger@hotmail.co.uk>
2015-11-26 19:06:30 +0000

    :eyes:Drawing world now (only base tiles)

--- build/script/app/Screen/GameScreen.js
+++ build/script/app/Screen/GameScreen.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "../request_animation_frame"], function (require, exports) {
+define(["require", "exports", "./GameScreen/WorldDrawing", "../request_animation_frame"], function (require, exports, WorldDrawing_1) {
     var GameScreen = (function () {
         function GameScreen(scope) {
             this.scope = scope;
@@ -6,15 +6,36 @@ define(["require", "exports", "../request_animation_frame"], function (require, 
             if (GameScreen.instance !== undefined) {
                 throw new Error("A GameScreen instance has already been constructed. Use GameScreen.get()");
             }
+            var world_canvas = scope.getElementById('world_canvas');
+            var player_canvas = scope.getElementById('players_canvas');
+            this.drawing = {
+                world: {
+                    canvas: world_canvas,
+                    context: world_canvas.getContext('2d')
+                },
+                players: {
+                    canvas: player_canvas,
+                    context: player_canvas.getContext('2d')
+                }
+            };
         }
         GameScreen.prototype.onShow = function () {
             console.log("showing game");
-            window.requestAnimationFrame(this.draw.bind(this));
-        };
-        GameScreen.prototype.draw = function () {
+            window.onresize += this.resized.bind(this);
+            this.resized();
             window.requestAnimationFrame(this.draw.bind(this));
         };
         GameScreen.prototype.onHide = function () {
+        };
+        GameScreen.prototype.draw = function () {
+            WorldDrawing_1.WorldDrawing.draw(this.drawing.world.canvas);
+            window.requestAnimationFrame(this.draw.bind(this));
+        };
+        GameScreen.prototype.resized = function () {
+            this.drawing.world.canvas.width = window.innerWidth;
+            this.drawing.world.canvas.height = window.innerHeight;
+            this.drawing.players.canvas.width = window.innerWidth;
+            this.drawing.players.canvas.height = window.innerHeight;
         };
         GameScreen.get = function (scope) {
             if (scope === void 0) { scope = undefined; }
--- src/ts/script/app/Screen/GameScreen.ts
+++ src/ts/script/app/Screen/GameScreen.ts
@@ -1,31 +1,57 @@
 ///<amd-dependency path="../request_animation_frame" />
 
 import { IScreen } from "./IScreen";
+import { WorldDrawing } from "./GameScreen/WorldDrawing";
 
 export class GameScreen implements IScreen {
   id = "game-screen";
   static instance: GameScreen;
+  drawing: any;
 
   constructor(private scope: HTMLDocument) {
     if (GameScreen.instance !== undefined) {
       throw new Error("A GameScreen instance has already been constructed. Use GameScreen.get()");
     }
+
+    var world_canvas: HTMLCanvasElement = <HTMLCanvasElement> scope.getElementById('world_canvas');
+    var player_canvas: HTMLCanvasElement = <HTMLCanvasElement> scope.getElementById('players_canvas');
+
+    this.drawing = {
+      world: {
+        canvas: world_canvas,
+        context: world_canvas.getContext('2d')
+      },
+      players: {
+        canvas: player_canvas,
+        context: player_canvas.getContext('2d')
+      }
+    };
   }
 
   onShow() {
     console.log("showing game");
 
-    window.requestAnimationFrame(this.draw.bind(this));
-  }
-
-  draw() {
-
+    window.onresize += this.resized.bind(this);
+    this.resized();
 
     window.requestAnimationFrame(this.draw.bind(this));
   }
 
   onHide() {
+    
+  }
 
+  draw() {
+    WorldDrawing.draw(this.drawing.world.canvas);
+
+    window.requestAnimationFrame(this.draw.bind(this));
+  }
+
+  resized() {
+    this.drawing.world.canvas.width = window.innerWidth;
+    this.drawing.world.canvas.height = window.innerHeight;
+    this.drawing.players.canvas.width = window.innerWidth;
+    this.drawing.players.canvas.height = window.innerHeight;
   }
 
   public static get(scope: HTMLDocument = undefined) {
